727fa2b7703c14716379fa23b5eaa9666b493119
keshav <keshavraj4546@gmail.com>
2023-04-26 17:45:53 +0530

    Added Segment

--- app/src/main/java/com/example/fragments/FragmentsActivity.kt
+++ app/src/main/java/com/example/fragments/FragmentsActivity.kt
@@ -1,11 +1,14 @@
 package com.example.fragments
 
+import android.R.attr.fragment
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 
+// Also use scope fun in this repo
 class FragmentsActivity : AppCompatActivity() {
 
     lateinit var buttonOne: Button
@@ -21,21 +24,30 @@ class FragmentsActivity : AppCompatActivity() {
         buttonOne= findViewById<Button>(R.id.firstSegment)
         buttonTwo = findViewById<Button>(R.id.secondSegment)
 
+
+
         buttonOne.setOnClickListener {
 
         }
 
         //
         buttonTwo.setOnClickListener {
-
         }
     }
 
     fun firstSegment(view: View) {
-
+        val manager: FragmentManager = supportFragmentManager
+        val transaction: FragmentTransaction = manager.beginTransaction()
+        transaction.add(R.id.frameLayout, FragmentOne())
+        transaction.commit()
     }
 
     fun SecondSegment(view: View) {
+
+        val manager: FragmentManager = supportFragmentManager
+        val transaction: FragmentTransaction = manager.beginTransaction()
+        transaction.add(R.id.frameLayout, FragmentSecond())
+        transaction.commit()
 
     }
 
